e843ca08dc2c717e1b05aec63577516ae873c39d
yujiseok <koesijuy@gmail.com>
2023-06-09 23:04:32 +0900

    feat: `SwapBlock` 컴포넌트 생성

---
+++ src/components/swap/SwapBlock.tsx
@@ -0,0 +1,8 @@
+const SwapBlock = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="rounded-2xl bg-uni-gray-5 p-4 text-uni-black-1">
+      {children}
+    </div>
+  );
+};
+export default SwapBlock;
