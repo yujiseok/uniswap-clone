2dd4f49968af208987e1a4dfd142974115bb9b32
yujiseok <koesijuy@gmail.com>
2023-06-09 18:20:46 +0900

    feat: 이더스캔 컴포넌트 생성

---
+++ src/components/Etherscan.tsx
@@ -0,0 +1,9 @@
+const Etherscan = () => {
+  return (
+    <div className="fixed bottom-4 right-4 flex items-center gap-1 text-[11px] text-uni-green-1">
+      <div className="opacity-70 hover:opacity-100">17434367</div>
+      <div className="inline-block h-2 w-2 rounded-full bg-uni-green-1" />
+    </div>
+  );
+};
+export default Etherscan;
