3253043e3ed870f4c402611f5504b1c3e53dabe8
yujiseok <koesijuy@gmail.com>
2023-06-13 17:00:33 +0900

    refactor: `formatter` 소수점 수정
    - 최대 소수점 5자리까지

--- src/lib/utils/formatter.ts
+++ src/lib/utils/formatter.ts
@@ -2,7 +2,7 @@ const formatter = new Intl.NumberFormat("en-US", {
   currency: "USD",
   style: "currency",
   minimumFractionDigits: 2,
-  maximumFractionDigits: 3,
+  maximumFractionDigits: 5,
 });
 
 export default formatter;
