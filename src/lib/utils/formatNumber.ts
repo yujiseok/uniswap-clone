ba523ecbb62811d9b6994e522e276f66c2aea234
yujiseok <koesijuy@gmail.com>
2023-06-13 16:07:03 +0900

    refactor: `formatter` 적용 및 주석 제거

--- src/lib/utils/formatNumber.ts
+++ src/lib/utils/formatNumber.ts
@@ -1,11 +1,7 @@
+import formatter from "./formatter";
+
 function formatNumber(stNumber: string, price: number) {
   const tokenPrice = Number(stNumber) * price;
-  const formatter = new Intl.NumberFormat("en-us", {
-    currency: "USD",
-    style: "currency",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 3,
-  });
 
   const units = [
     {
@@ -21,16 +17,6 @@ function formatNumber(stNumber: string, price: number) {
       symbol: "M",
     },
   ];
-
-  // if (tokenPrice >= 1000000000000) {
-  //   return "$" + (tokenPrice / 1000000000000).toFixed(2) + "T";
-  // }
-  // if (tokenPrice >= 1000000000) {
-  //   return "$" + (tokenPrice / 1000000000).toFixed(2) + "B";
-  // }
-  // if (tokenPrice >= 1000000) {
-  //   return "$" + (tokenPrice / 1000000).toFixed(2) + "M";
-  // }
 
   for (const unit of units) {
     if (tokenPrice >= unit.value) {
