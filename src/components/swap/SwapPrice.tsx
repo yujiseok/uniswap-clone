14d29b54d13bea47e6026e59474c52c7cf03ddf1
yujiseok <koesijuy@gmail.com>
2023-06-14 12:50:29 +0900

    feat: 스타일링 추가

--- src/components/swap/SwapPrice.tsx
+++ src/components/swap/SwapPrice.tsx
@@ -1,4 +1,4 @@
 const SwapPrice = ({ children }: { children: React.ReactNode }) => {
-  return <div className="min-h-[20px] pt-2 text-sm">{children}</div>;
+  return <div className="min-h-[22px] pt-2 text-sm">{children}</div>;
 };
 export default SwapPrice;
